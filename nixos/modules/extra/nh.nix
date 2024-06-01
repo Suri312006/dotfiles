be87eac93b14bf8064918568926529fadd8aadf8
suri <suri312006@gmail.con>
2024-06-01 01:31:10 -0700

    nh

--- nixos/modules/extra/nh.nix
+++ nixos/modules/extra/nh.nix
@@ -1,13 +1,12 @@
-{...}: {
-  #TODO: figure out how to do this
-  # programs.nh = {
-  #   enable = true;
-  #   package = inputs.nh.packages.x86_64-linux.default;
-  #   clean.enable = true;
-  #   clean.extraArgs = "--keep-since 4d --keep 3";
-  #   #TODO: figure out a way to make this not hard coded
-  #   flake = "/home/suri/dotfiles";
-  # };
+{ inputs, pkgs, ... }: {
+  programs.nh = {
+    enable = true;
+    package = inputs.nh.packages.x86_64-linux.default;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    #TODO: figure out a way to make this not hard coded
+    flake = "/home/suri/dotfiles";
+  };
 
 }
 
