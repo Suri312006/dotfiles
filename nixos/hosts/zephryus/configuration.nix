df063d14c9bc03a7c9d0e6415f05d772b48e4596
suri <suri312006@gmail.con>
2024-06-01 01:00:31 -0700

    what about this?

--- nixos/hosts/zephryus/configuration.nix
+++ nixos/hosts/zephryus/configuration.nix
@@ -78,7 +78,7 @@
   # FIXME: Add the rest of your current configuration
 
   # TODO: Set your hostname
-  networking.hostName = "your-hostname";
+  # networking.hostName = "your-hostname";
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
@@ -99,16 +99,16 @@
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
-  services.openssh = {
-    enable = true;
-    settings = {
-      # Opinionated: forbid root login through SSH.
-      PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
-      PasswordAuthentication = false;
-    };
-  };
+  # services.openssh = {
+  #   enable = true;
+  #   settings = {
+  #     # Opinionated: forbid root login through SSH.
+  #     PermitRootLogin = "no";
+  #     # Opinionated: use keys only.
+  #     # Remove if you want to SSH using passwords
+  #     PasswordAuthentication = false;
+  #   };
+  # };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
--- nixos/modules/core/nixconfig.nix
+++ nixos/modules/core/nixconfig.nix
@@ -1,21 +1,61 @@
 # holds the core nixos configuration for the system
-{ inputs, pkgs, user, ... }: {
-  nix = {
-    settings = {
-      auto-optimise-store = true;
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "root" "@wheel" ];
-    };
-  };
+{ inputs, pkgs, config, ... }: {
+  # nix = {
+  #   settings = {
+  #     auto-optimise-store = true;
+  #     experimental-features = [ "nix-command" "flakes" ];
+  #     trusted-users = [ "root" "@wheel" ];
+  #   };
+  # };
+  #
+  # nixpkgs = {
+  #   config = {
+  #     # lookinto later
+  #     allowBroken = true;
+  #     allowUnsupportedSystem = true;
+  #     allowUnfree = true;
+  #   };
+  # };
 
   nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
     config = {
-      # lookinto later
-      allowBroken = true;
-      allowUnsupportedSystem = true;
+      # Disable if you don't want unfree packages
       allowUnfree = true;
     };
   };
 
+  nix =
+    let
+      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    in
+    {
+      settings = {
+        # Enable flakes and new 'nix' command
+        experimental-features = "nix-command flakes";
+        # Opinionated: disable global registry
+        flake-registry = "";
+        # Workaround for https://github.com/NixOS/nix/issues/9574
+        nix-path = config.nix.nixPath;
+      };
+      # Opinionated: disable channels
+      channel.enable = false;
+
+      # Opinionated: make flake registry and nix path match flake inputs
+      registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    };
 
 }
