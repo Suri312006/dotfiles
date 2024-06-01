1f922c6d15c4e145fafcc352c19cf9c5c4d90af6
suri <suri312006@gmail.con>
2024-06-01 14:33:16 -0700

    nh not working

--- nixos/modules/extra/nh.nix
+++ nixos/modules/extra/nh.nix
@@ -1,7 +1,7 @@
-{ inputs, pkgs, ... }: {
+{...}: {
   programs.nh = {
     enable = true;
-    package = inputs.nh.packages.x86_64-linux.default;
+    # package = inputs.nh.packages.x86_64-linux.default;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
     #TODO: figure out a way to make this not hard coded
