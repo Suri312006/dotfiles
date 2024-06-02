c5654420b3d2e294af626d51c127e7069956f7fc
suri <suri312006@gmail.con>
2024-06-02 00:35:55 -0700

    this is guaranteed to fail

--- home-manager/modules/hyprland/default.nix
+++ home-manager/modules/hyprland/default.nix
@@ -3,4 +3,11 @@
     ./hypr.nix
     ./scripts.nix
   ];
+  # make stuff work on wayland
+  home.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
+    SDL_VIDEODRIVER = "wayland";
+    XDG_SESSION_TYPE = "wayland";
+    QT_SCALE_FACTOR_ROUNDING_POLICY = "RoundPreferFloor";
+  };
 }
--- home-manager/modules/rice/gtk.nix
+++ home-manager/modules/rice/gtk.nix
@@ -1,52 +1,61 @@
-{ pkgs, ... }: {
-  home.sessionVariables = { GTK_THEME = "Nordic"; };
+{ inputs
+, pkgs
+, config
+, ...
+}: {
   home.pointerCursor = {
-    name = "Catppuccin-Mocha-Dark-Cursors";
-    package = pkgs.catppuccin-cursors.mochaDark;
-    size = 16;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Original-Ice";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
   };
+
   gtk = {
     enable = true;
-    theme = {
-      name = "Nordic";
-      package = pkgs.nordic;
-    };
-    cursorTheme = {
-      name = "Catppuccin-Mocha-Dark-Cursors";
-      package = pkgs.catppuccin-cursors.mochaDark;
-    };
-    iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-icon-theme;
-    };
     font = {
-      name = "JetBrainsMono Nerd Font";
+      name = "SF Pro Display";
+      package = inputs.self.packages.${pkgs.system}.SF-Pro;
       size = 11;
     };
-    gtk3.extraConfig = {
-      gtk-xft-antialias = 1;
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintslight";
-      gtk-xft-rgba = "rgb";
-      gtk-cursor-theme-name = "Catppuccin-Mocha-Green-Cursors";
+
+    gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
+    iconTheme = {
+      name = "Colloid";
+      package = pkgs.colloid-icon-theme;
     };
+
+    theme = {
+      name = "WhiteSur-Dark";
+      package = pkgs.whitesur-gtk-theme.override {
+        nautilusStyle = "glassy";
+      };
+    };
+    gtk3 = {
+      bookmarks = [
+        "file://${config.home.homeDirectory}/Documents"
+        "file://${config.home.homeDirectory}/Downloads"
+        "file://${config.home.homeDirectory}/Music"
+        "file://${config.home.homeDirectory}/Pictures"
+        "file://${config.home.homeDirectory}/Videos"
+      ];
+      extraConfig = {
+        gtk-xft-antialias = 1;
+        gtk-xft-hinting = 1;
+        gtk-xft-hintstyle = "hintfull";
+        gtk-xft-rgba = "rgb";
+        gtk-application-prefer-dark-theme = 1;
+      };
+    };
+
     gtk2.extraConfig = ''
       gtk-xft-antialias=1
       gtk-xft-hinting=1
       gtk-xft-hintstyle="hintslight"
       gtk-xft-rgba="rgb"
     '';
-    gtk4.extraConfig = {
-      gtk-xft-antialias = 1;
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintslight";
-      gtk-xft-rgba = "rgb";
-      gtk-cursor-theme-name = "Catppuccin-Mocha-Green-Cursors";
-    };
-  };
 
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk";
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
   };
 }
