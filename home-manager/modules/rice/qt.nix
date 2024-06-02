a547b44f1a38952e0c7b076ead65632a04b454a0
suri <suri312006@gmail.con>
2024-06-02 00:26:23 -0700

    i hate qt

--- home-manager/modules/rice/qt.nix
+++ home-manager/modules/rice/qt.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }: {
-  qt.enable = true;
-  qt.platformTheme.name = "qt5ct";
-  # qt.style.name = "dracula theme";
-  qt.style.package = pkgs.dracula-theme;
-
-  home.packages = [ pkgs.libsForQt5.qt5ct ];
-}
+{}
+# { pkgs, ... }: {
+#   qt.enable = true;
+#   qt.platformTheme.name = "qt5ct";
+#   # qt.style.name = "dracula theme";
+#   qt.style.package = pkgs.dracula-theme;
+#
+#   home.packages = [ pkgs.libsForQt5.qt5ct ];
+# }
