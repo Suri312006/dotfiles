052019681957b4c53e46af59e8ee876c812678dc
suri <suri312006@gmail.con>
2024-06-02 15:30:03 -0700

    some gtk things

--- home-manager/modules/rice/gtk.nix
+++ home-manager/modules/rice/gtk.nix
@@ -1,28 +1,34 @@
 { pkgs, ... }: {
   home.sessionVariables = { GTK_THEME = "Nordic"; };
+
   home.pointerCursor = {
     name = "Catppuccin-Mocha-Dark-Cursors";
     package = pkgs.catppuccin-cursors.mochaDark;
     size = 16;
   };
+
   gtk = {
     enable = true;
     theme = {
       name = "Nordic";
       package = pkgs.nordic;
     };
+
     cursorTheme = {
       name = "Catppuccin-Mocha-Dark-Cursors";
       package = pkgs.catppuccin-cursors.mochaDark;
     };
+
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
+
     font = {
       name = "JetBrainsMono Nerd Font";
       size = 11;
     };
+
     gtk3.extraConfig = {
       gtk-xft-antialias = 1;
       gtk-xft-hinting = 1;
@@ -30,6 +36,7 @@
       gtk-xft-rgba = "rgb";
       gtk-cursor-theme-name = "Catppuccin-Mocha-Green-Cursors";
     };
+
     gtk2.extraConfig = ''
       gtk-xft-antialias=1
       gtk-xft-hinting=1
