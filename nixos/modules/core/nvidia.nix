231a393b18ef63fea3f149539e0690dba77b6c23
suri <suri312006@gmail.con>
2024-06-01 19:57:38 -0700

    disable supergfx

--- nixos/modules/core/nvidia.nix
+++ nixos/modules/core/nvidia.nix
@@ -24,17 +24,17 @@
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
-  services.supergfxd = {
-    enable = true;
-    settings = {
-      # mode = "Integrated";
-      vfio_enable = true;
-      vfio_save = false;
-      always_reboot = false;
-      no_logind = false;
-      logout_timeout_s = 20;
-      hotplug_type = "Asus";
-    };
-  };
+  # services.supergfxd = {
+  #   enable = true;
+  #   settings = {
+  #     # mode = "Integrated";
+  #     vfio_enable = true;
+  #     vfio_save = false;
+  #     always_reboot = false;
+  #     no_logind = false;
+  #     logout_timeout_s = 20;
+  #     hotplug_type = "Asus";
+  #   };
+  # };
 
 }
