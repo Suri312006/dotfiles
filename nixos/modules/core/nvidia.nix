00c8a5724f4780ab39ad017223113e53c8c2a841
suri <suri312006@gmail.con>
2024-06-01 20:05:54 -0700

    what about this now

--- nixos/modules/core/nvidia.nix
+++ nixos/modules/core/nvidia.nix
@@ -1,12 +1,15 @@
 { pkgs, config, ... }: {
 
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
   hardware = {
     opengl.enable = true;
 
     nvidia = {
       modesetting.enable = true;
-      powerManagement.enable = true;
-      powerManagement.finegrained = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
 
       open = false;
       nvidiaSettings = true;
@@ -16,6 +19,12 @@
     };
   };
 
+  hardware.nvidia.prime.sync.enable = true;
+
+  hardware.nvidia.prime = {
+    nvidiaBusId = "PCI:01:00:0"; # Found with lspci | grep VGA
+    amdgpuBusId = "PCI:07:00:0"; # Found with lspci | grep VGA
+  };
 
 
   #NOTE: not sure this is in the right spot
