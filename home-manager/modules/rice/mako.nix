6cabb65875e54b85d89563ee26e15f6f052e45d0
suri <suri312006@gmail.con>
2024-06-08 00:14:56 -0700

    man why am i so silly

--- home-manager/modules/rice/mako.nix
+++ home-manager/modules/rice/mako.nix
@@ -4,11 +4,11 @@ let
   suri_toggle_dnd = pkgs.writeShellScriptBin "suri_toggle_dnd" ''
     NOTIF_TOGGLE=$HOME/.config/.notiftoggle
 
-    if [ ! -e $TOGGLE ]; then 
-      touch $TOGGLE
+    if [ ! -e $NOTIF_TOGGLE ]; then 
+      touch $NOTIF_TOGGLE
       makoctl set-mode dnd
     else 
-      rm $TOGGLE
+      rm $NOTIF_TOGGLE
       makoctl set-mode default
     fi
   '';
