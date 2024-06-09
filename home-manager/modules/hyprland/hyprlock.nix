c361b4bff8dd9f3275e080d1e3ef6c6efdd67a4a
suri <suri312006@gmail.con>
2024-06-08 18:55:39 -0700

    hyprlock

--- home-manager/modules/hyprland/hyprlock.nix
+++ home-manager/modules/hyprland/hyprlock.nix
@@ -1,9 +1,11 @@
-{config, ...}: let
+{ config, ... }:
+let
   # variant = config.theme.name;
   # c = config.programs.matugen.theme.colors.colors.${variant};
 
   font_family = "GeistMono Nerd Font";
-in {
+in
+{
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -16,22 +18,24 @@ in {
       background = [
         {
           monitor = "";
-          path = "${config.xdg.configHome}/hyprlock.png";
+          path = "$HOME/Pictures/Wallpapers/lol.png";
           blur_passes = 3;
-          blur_size = 6;
-          noise = 0.1;
+          # blur_size = 6;
+          # noise = 0.1;
           contrast = 1.1;
-          brightness = 1.2;
+          brightness = 0.8172;
+          vibrancy = 0.1696;
+          vibrancy_darkness = 0.0;
         }
       ];
 
       input-fields = [
         {
-          monitor = "eDP-1";
+          # monitor = "eDP-1";
 
           size = {
-            width = 300;
-            height = 50;
+            width = 250;
+            height = 60;
           };
 
           outline_thickness = 2;
@@ -48,21 +52,39 @@ in {
         }
       ];
 
+      # time
       labels = [
         {
           monitor = "";
-          text = "$TIME";
-          inherit font_family;
-          font_size = 50;
+          # text = "$TIME";
+          text = ''cmd[update:1000] echo "$(date +"%-I:%M%p")"'';
+          # inherit font_family;
+          font_size = 120;
+          font_family = "JetBrains Mono Nerd Font Mono";
           # color = "rgb(${c.primary})";
 
           position = {
             x = 0;
-            y = 80;
+            y = -300;
           };
 
-          valign = "center";
+          valign = "top";
           halign = "center";
+        }
+        {
+          # monitor =
+          text = "Hi there, $USER";
+          color = "$foreground ";
+          #color = rgba(255, 255, 255, 0.6)
+          font_size = 25;
+          font_family = "JetBrains Mono Nerd Font Mono";
+          position = {
+            x = 0;
+            y = -40;
+          };
+          halign = "center";
+          valign = "center";
+
         }
       ];
     };
