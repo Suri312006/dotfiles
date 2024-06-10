ed9ddf5117896bb97c9ff746d8e980856e6db108
suri <suri312006@gmail.con>
2024-06-09 23:01:41 -0700

    Generation: '440'

--- modules/home/hyprland/hypr.nix
+++ modules/home/hyprland/hypr.nix
@@ -100,7 +100,8 @@
           "$mod, S, exec, firefox"
           "$mod, D, exec, discord"
           "$mod, Return, exec, kitty"
-          "$mod, escape, exec, pkill wofi || wofi --show drun -modi drun,filebrowser,run,window"
+          # "$mod, escape, exec, pkill wofi || wofi --show drun -modi drun,filebrowser,run,window"
+          "$mod, escape, exec, ags --toggle-window applauncher"
           # "$mod, escape, exec, pkill rofi || rofi -show drun -modi drun,filebrowser,run,window"
           "$mod, T, exec, thunar"
 
