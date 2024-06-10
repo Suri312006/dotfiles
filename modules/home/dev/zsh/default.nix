6058d953ef68c149c7beee2c754a9d7d6700a5a4
suri <suri312006@gmail.con>
2024-06-09 17:14:07 -0700

    Generation: '431'

--- modules/home/dev/zsh/default.nix
+++ modules/home/dev/zsh/default.nix
@@ -50,7 +50,7 @@
         VERSION=$(($(readlink /nix/var/nix/profiles/system | grep -o "[0-9]*") + 1)) 
         z ~/dots/nixdots && git add -A && git commit -m "Generation: '$VERSION'" && sudo nixos-rebuild switch --flake /home/suri/dots/nixdots#zephryus'';
 
-      ree = ''sudo nixos-rebuild switch --flake ~/dots/nixdots#zephrryus'';
+      ree = ''sudo nixos-rebuild switch --flake ~/dots/nixdots#zephryus'';
 
       dots = "z ~/dots";
       fcd = ''cd "$(find ~/coding/ ~/storage/ -type d -not \( -path "*/.git/*" -o -path "*/target/*" -o -path "*/.venv/*" -o -path "*/node_modules/*" -o -path "*/venv/*" -o -path "*/build/*" -o -path "*/.*/*" \) -print 2>/dev/null | fzf)" '';
