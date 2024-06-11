067c68dd8e10be4062f43b06c13144a6429002d6
suri <suri312006@gmail.con>
2024-06-11 16:19:42 -0700

    Generation: '532'

--- modules/system/extra/stylix.nix
+++ modules/system/extra/stylix.nix
@@ -1,6 +1,15 @@
 { pkgs, inputs, ... }:
+let
+
+  wallpaper = pkgs.fetchurl {
+    url = "http://downloads.bungie.net/images/Wallpapers/S19/1920x1080/d2_S19_Community_Wallpapers_1920x1080_10.jpg";
+    sha256 = "sha256-bjmbSBEaQZgbu1dBGFqU5fClqrU2J2qMgJ4Ha67X7jw=";
+  };
+in
 {
   # home.file."~/Pictures/Wallpaper.png" = wallpaper;
+
+  stylix.image = wallpaper;
 
   stylix = {
     enable = true;
