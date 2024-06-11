3c9b4ab07ec73c713f315f7ae7179e7fad799a08
suri <suri312006@gmail.con>
2024-06-11 16:03:48 -0700

    Generation: '531'

--- modules/home/hyprland/hypr.nix
+++ modules/home/hyprland/hypr.nix
@@ -8,8 +8,8 @@
 
 
       exec-once = [
-        # "swww query || swww-daemon --format xrgb"
-        # "swww $HOME/Pictures/Wallpapers/lol.png"
+        "swww query || swww-daemon --format xrgb"
+        "swww $HOME/Pictures/Wallpaper.png"
         # Startup
         "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
--- modules/system/extra/stylix.nix
+++ modules/system/extra/stylix.nix
@@ -5,8 +5,13 @@
   stylix = {
     enable = true;
     # image = ~/Pictures/Wallpapers/lol.png;
-    image = inputs.wallpaper;
+    # image = inputs.wallpaper;
     polarity = "dark";
+
+    image = pkgs.fetchurl {
+      url = "http://downloads.bungie.net/images/Wallpapers/S19/1920x1080/d2_S19_Community_Wallpapers_1920x1080_10.jpg";
+      sha256 = "sha256-bjmbSBEaQZgbu1dBGFqU5fClqrU2J2qMgJ4Ha67X7jw=";
+    };
 
     fonts = {
       serif = {
