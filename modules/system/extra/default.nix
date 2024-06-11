5c43cd052f52356f331bef16498432c550bae33a
suri <suri312006@gmail.con>
2024-06-11 10:42:01 -0700

    Generation: '524'

--- modules/system/extra/default.nix
+++ modules/system/extra/default.nix
@@ -4,6 +4,7 @@
     ./fonts.nix
     ./print.nix
     ./nh.nix
+    ./stylix.nix
   ];
 
 }
---
+++ modules/system/extra/stylix.nix
@@ -0,0 +1,35 @@
+
+{ pkgs, ... }: {
+  stylix = {
+    enable = true;
+    # image = ~/Pictures/Wallpapers/lol.png;
+    image = pkgs.fetchurl {
+      url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
+      sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    };
+    polarity = "dark";
+
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans Mono";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+  };
+
+}
