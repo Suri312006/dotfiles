09da8854d06ec36841279590c32b76a5ef52781a
suri <suri312006@gmail.con>
2024-06-12 10:55:01 -0700

    Generation: '569'

--- modules/home/dev/kitty/default.nix
+++ modules/home/dev/kitty/default.nix
@@ -5,7 +5,7 @@
     # font = {
       #   package = pkgs.fira-code;
       #   name = "Fira Code SemiBold";
-      # size = lib.mkForce 9.5;
+      size = lib.mkForce 9.5;
     # };
     settings = {
       enable_audio_bell = "no";
