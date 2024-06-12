3c47b900c3fee25d0fa81b2025b981d3bb08a0ac
suri <suri312006@gmail.con>
2024-06-11 21:02:08 -0700

    Generation: '563'

--- modules/system/extra/stylix.nix
+++ modules/system/extra/stylix.nix
@@ -13,8 +13,8 @@
     #   sha256 = "sha256-bjmbSBEaQZgbu1dBGFqU5fClqrU2J2qMgJ4Ha67X7jw=";
     # };
 
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-hard.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-material-dark-medium.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox--dark-hard.yaml";
 
     cursor = {
       package = pkgs.bibata-cursors;
