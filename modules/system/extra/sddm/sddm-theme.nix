745863d3e7c062f518f377b5e0fc3957b3f2957d
suri <suri312006@gmail.con>
2024-06-12 17:11:35 -0700

    Generation: '571'

--- modules/system/extra/sddm/sddm-theme.nix
+++ modules/system/extra/sddm/sddm-theme.nix
@@ -8,7 +8,7 @@ pkgs.stdenv.mkDerivation {
     owner = "MarianArlt";
     repo = "sddm-sugar-dark";
     rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    hash = "sha256-flOspjpYezPvGZ6b4R/Mr18N7N3JdytCSwwu6mf4owQ-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   };
 
   installPhase = ''
