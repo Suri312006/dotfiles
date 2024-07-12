5206882a140d6383ecfec7b2f46cac23ec5be8a6
suri312006 <suri312006@gmail.com>
2024-07-11 22:08:51 -0700

    Generation: '619'

--- modules/home/hyprland/hypr.nix
+++ modules/home/hyprland/hypr.nix
@@ -33,8 +33,8 @@
 
       monitor = [
         "eDP-1, 2560x1440@165.00, 0x0, 1.6"
-        "DP-3, 1920x1080@74.97, 1600x0, 1"
-        "DP-4, preferred, auto, 1, transform, 1"
+        "DP-4, 1920x1080@74.97, 1600x0, 1"
+        "DP-3, preferred, auto, 1, transform, 1"
       ];
 
       # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
@@ -176,17 +176,17 @@
       ];
 
       workspace = [
-        "1, monitor:DP-3"
-        "2, monitor:DP-3"
-        "3, monitor:DP-3"
-        "4, monitor:DP-3"
-        "5, monitor:DP-3"
+        "1, monitor:DP-4"
+        "2, monitor:DP-4"
+        "3, monitor:DP-4"
+        "4, monitor:DP-4"
+        "5, monitor:DP-4"
 
         "6, monitor:eDP-1"
         "7, monitor:eDP-1"
         "8, monitor:eDP-1"
         "9, monitor:eDP-1"
-        "10, monitor:DP-4"
+        "10, monitor:DP-3"
       ];
 
       dwindle = {
