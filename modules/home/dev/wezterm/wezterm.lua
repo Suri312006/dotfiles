8cf48394db03749a1578ec9236a1b7736da44da8
suri312006 <suri312006@gmail.com>
2024-07-14 11:33:43 -0700

    Generation: '671'

--- modules/home/dev/wezterm/wezterm.lua
+++ modules/home/dev/wezterm/wezterm.lua
@@ -1,3 +1,4 @@
+
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
@@ -14,7 +15,7 @@ config.color_scheme = 'Gruvbox dark, hard (base16)';
 config.font_size = 10.5;
 config.warn_about_missing_glyphs = true
 config.freetype_load_target =
-'HorizontalLcd'                               -- https://wezfurlong.org/wezterm/config/lua/config/freetype_load_target.html
+'HorizontalLcd' -- https://wezfurlong.org/wezterm/config/lua/config/freetype_load_target.html
 
 config.hide_tab_bar_if_only_one_tab = true;
 
@@ -68,21 +69,113 @@ config.font_rules = {
 -- keybinds
 config.keys = {
     {
-        key = 'V',
+        key = 'v',
         mods = 'CTRL',
         action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
 
     },
     {
-        key = 'X',
+        key = 'x',
         mods = 'CTRL',
         action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
     },
     {
-        key = 'S',
+        key = 's',
         mods = 'CTRL',
-        action = wezterm.action.ShowLauncher,
+        action = wezterm.action.ShowLauncherArgs { flags = "WORKSPACES|DOMAINS|FUZZY" },
     }
 }
+
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+    -- allow `wezterm start -- something` to affect what we spawn
+    -- in our initial window
+    local args = {}
+    if cmd then
+        args = cmd.args
+    end
+
+    -- Set a workspace for coding on a current project
+    -- Top pane is for the editor, bottom pane is for the build tool
+    -- local project_dir = wezterm.home_dir .. '/wezterm'
+    -- local tab, build_pane, window = mux.spawn_window {
+    --     workspace = 'coding',
+    --     cwd = project_dir,
+    --     args = args,
+    -- }
+    -- local editor_pane = build_pane:split {
+    --     direction = 'Top',
+    --     size = 0.6,
+    --     cwd = project_dir,
+    -- }
+    -- -- may as well kick off a build in that pane
+    -- build_pane:send_text 'cargo build\n'
+    --
+    -- -- A workspace for interacting with a local machine that
+    -- -- runs some docker containners for home automation
+    -- local tab, pane, window = mux.spawn_window {
+    --     workspace = 'automation',
+    --     args = { 'ssh', 'vault' },
+    -- }
+
+
+
+    local school_dir = wezterm.home_dir .. '/dev/school'
+    local tab, pane, window = mux.spawn_window {
+        workspace = 'school',
+        cwd = school_dir,
+        args = args
+    }
+
+    local default_dir = wezterm.home_dir .. '/dev'
+    local tab, pane, window = mux.spawn_window {
+        workspace = 'default',
+        cwd = default_dir,
+        args = args
+    }
+
+    local connectify_dir = wezterm.home_dir .. '/dev/work/ConnectifyAI'
+    local tab, pane, window = mux.spawn_window {
+        workspace = 'connectify',
+        cwd = connectify_dir,
+        args = args
+    }
+
+    local build_tab, build_pane, build_window = window:spawn_tab {}
+    local dev_server = build_pane:split {
+        direction = 'Left',
+        size = 0.5,
+        cwd = connectify_dir .. '/connectifyai/backend/cmd/connserver'
+    }
+
+    build_tab:set_title 'build'
+
+    dev_server:send_text 'go run .\n'
+
+    tab:activate()
+
+
+    -- We want to startup in the coding workspace
+    mux.set_active_workspace 'default'
+end)
+
+
+-- config.disable_default_key_bindings = true;
+
+-- config.keys = {
+--     -- This will create a new split and run the `top` program inside it
+--     {
+--         key = '%',
+--         mods = 'CTRL|SHIFT|ALT',
+--         action = wezterm.action.SplitPane {
+--             direction = 'Left',
+--             -- command = { args = { 'top' } },
+--             size = { Percent = 50 },
+--         },
+--
+--     },
+--
+-- }
 -- and finally, return the configuration to wezterm
 return config
