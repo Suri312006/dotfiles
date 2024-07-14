582817781ca89e08d31433af6863a56652737f64
suri312006 <suri312006@gmail.com>
2024-07-14 13:48:48 -0700

    Generation: '680'

--- modules/home/dev/wezterm/wezterm/binds.lua
+++ modules/home/dev/wezterm/wezterm/binds.lua
@@ -2,17 +2,42 @@ local wezterm = require 'wezterm'
 
 local module = {}
 
+-- Show which key table is active in the status area
+wezterm.on('update-right-status', function(window, pane)
+    local name = window:active_key_table()
+    if name then
+        name = 'TABLE: ' .. name
+    end
+    window:set_right_status(name or '')
+end)
 
 
 function module.apply_to_config(config)
     local act = wezterm.action;
 
+
     config.keys = {
+        {
+            key = 'p',
+            mods = 'ALT',
+            action = act.ActivateKeyTable {
+                name = 'pane',
+                timeout_milliseconds = 1000
+            }
+        },
+        {
+            key = 't',
+            mods = 'ALT',
+            action = act.ActivateKeyTable {
+                name = 'tab',
+                timeout_milliseconds = 1000
+            },
+        },
+
         {
             key = 'v',
             mods = 'ALT',
-            action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }
-
+            action = act.SplitVertical { domain = 'CurrentPaneDomain' }
         },
         {
             key = 'x',
@@ -24,29 +49,67 @@ function module.apply_to_config(config)
             mods = 'ALT',
             action = act.ShowLauncherArgs { flags = "WORKSPACES|DOMAINS|FUZZY" },
         },
-        {
-            key = 'p',
-            mods = 'ALT',
-            action = act.PaneSelect {
-                alphabet = 'arstgmneio'
-            },
-        },
+        -- {
+        --     key = 'p',
+        --     mods = 'ALT',
+        --     action = act.PaneSelect {
+        --         alphabet = 'arstgmneio'
+        --     },
+        -- },
 
         {
             key = 'q',
-            mods = 'SUPER',
+            mods = 'ALT',
             action = act.QuitApplication
         },
-        {
-            key = 'q',
-            mods = 'ALT',
-            action = act.CloseCurrentPane { confirm = true }
+        -- {
+        --     key = 'q',
+        --     mods = 'ALT',
+        --     action = act.CloseCurrentPane { confirm = true }
+        -- },
+        -- {
+        --     key = 'q',
+        --     mods = 'CTRL',
+        --     action = act.CloseCurrentTab { confirm = true }
+        -- }
+    }
+
+    config.key_tables = {
+        pane = {
+            {
+                key = 'n',
+                action = act.SplitPane {
+                    direction = 'right',
+                    size = { Percent = 50 },
+                }
+            },
+            {
+                key = 'q',
+                action = act.CloseCurrentPane { confirm = true }
+            },
+            {
+
+                key = 's',
+                action = act.PaneSelect {
+                    alphabet = 'arstgmneio'
+                },
+            }
+            -- TODO:maybe add a way to resize pane? nested key tables
         },
-        {
-            key = 'q',
-            mods = 'CTRL',
-            action = act.CloseCurrentTab { confirm = true }
+        tab = {
+            {
+                key = 'n',
+                action = act.SpawnTab 'CurrentPaneDomain'
+            },
+            {
+                key = 'q',
+                action = act.CloseCurrentTab { confirm = true }
+            },
+
+
         }
+
+
     }
 end
 
