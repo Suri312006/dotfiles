6a8a37563ee2703f37403ddd3df92ffa38397afb
suri312006 <suri312006@gmail.com>
2024-07-13 23:20:01 -0700

    Generation: '653'

--- modules/home/dev/kitty/default.nix
+++ modules/home/dev/kitty/default.nix
@@ -6,7 +6,7 @@
       #   package = pkgs.fira-code;
         # name = "Fira Code SemiBold";
         package = lib.mkForce pkgs.monaspace;
-        name = lib.mkForce "Monaspace Krypton Var";
+        name = lib.mkForce "Monaspace Krypton Var Bold";
         size = lib.mkForce 10;
         weight = "Bold";
     };
