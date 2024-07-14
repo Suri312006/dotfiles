294194477fa0db5203587cf3113677c6048ed957
suri312006 <suri312006@gmail.com>
2024-07-14 12:54:00 -0700

    Generation: '679'

--- modules/home/dev/wezterm/wezterm/binds.lua
+++ modules/home/dev/wezterm/wezterm/binds.lua
@@ -5,24 +5,48 @@ local module = {}
 
 
 function module.apply_to_config(config)
+    local act = wezterm.action;
+
     config.keys = {
         {
             key = 'v',
             mods = 'ALT',
-            action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+            action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }
 
         },
         {
             key = 'x',
             mods = 'ALT',
-            action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+            action = act.SplitVertical { domain = 'CurrentPaneDomain' }
         },
         {
             key = 's',
             mods = 'ALT',
-            action = wezterm.action.ShowLauncherArgs { flags = "WORKSPACES|DOMAINS|FUZZY" },
+            action = act.ShowLauncherArgs { flags = "WORKSPACES|DOMAINS|FUZZY" },
         },
-        { key = 'q', mods = 'ALT', action = wezterm.action.QuitApplication },
+        {
+            key = 'p',
+            mods = 'ALT',
+            action = act.PaneSelect {
+                alphabet = 'arstgmneio'
+            },
+        },
+
+        {
+            key = 'q',
+            mods = 'SUPER',
+            action = act.QuitApplication
+        },
+        {
+            key = 'q',
+            mods = 'ALT',
+            action = act.CloseCurrentPane { confirm = true }
+        },
+        {
+            key = 'q',
+            mods = 'CTRL',
+            action = act.CloseCurrentTab { confirm = true }
+        }
     }
 end
 
