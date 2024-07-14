a0d4a4e7908bcfa09dd7b26dc660092baca7f4e0
suri312006 <suri312006@gmail.com>
2024-07-13 20:32:05 -0700

    Generation: '632'

---
+++ modules/home/dev/wezterm/default.nix
@@ -0,0 +1,13 @@
+{...}:{
+
+  programs.wezterm = {
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+
+    extraConfig = builtins.readFile ./wezterm.lua;
+  };
+  
+  # home.file.".config/wezterm/wezterm.lua".source = ./wezterm.lua;
+
+  }
