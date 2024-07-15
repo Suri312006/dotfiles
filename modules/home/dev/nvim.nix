49b54fcc783cb27dbaceb62b5729a996db5a6b5d
suri312006 <suri312006@gmail.com>
2024-07-15 15:55:02 -0700

    Generation: '689'

--- modules/home/dev/nvim.nix
+++ modules/home/dev/nvim.nix
@@ -18,7 +18,7 @@
   home.file.".config/nvim" = {
     source = builtins.fetchGit {
       url = "https://github.com/suri312006/nvim";
-      rev = "d4efd841bc5290320b07742f140d97846c65b7fc";
+      rev = "67ddab93712b412428360c70d7cd5a033c4b1327";
     };
   };
 
