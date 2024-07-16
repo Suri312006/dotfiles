45fedaf38e9c31e81cce82706a10a49d72b3e49f
suri312006 <suri312006@gmail.com>
2024-07-16 02:38:47 -0700

    Generation: '701'

--- modules/home/dev/wezterm/wezterm/workspaces/defualt.lua
+++ modules/home/dev/wezterm/wezterm/workspaces/defualt.lua
@@ -1,16 +1,23 @@
 local wezterm = require 'wezterm'
+
 local mux = wezterm.mux
+
+
 wezterm.on('gui-startup', function(cmd)
+    -- allow `wezterm start -- something` to affect what we spawn
+    -- in our initial window
     local args = {}
     if cmd then
         args = cmd.args
     end
+    local home = wezterm.home_dir
 
-    local tab, pane, window = mux:spawn_window {
-        workspace = 'default',
-        cwd = wezterm.home_dir .. '',
+    local tab, pane, window = mux.spawn_window {
+        workspace = 'school',
+        cwd = home,
         args = args
     }
-
     mux.set_active_workspace 'default'
 end)
+
+return {}
