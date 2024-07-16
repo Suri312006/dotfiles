c871aee7c6648a8ed5a9405d18a1e80234ede08e
suri312006 <suri312006@gmail.com>
2024-07-16 03:12:41 -0700

    Generation: '704'

--- modules/home/dev/wezterm/wezterm/workspaces/init.lua
+++ modules/home/dev/wezterm/wezterm/workspaces/init.lua
@@ -2,5 +2,6 @@
 require('workspaces.school')
 require('workspaces.dots')
 require('workspaces.connectify')
+require('workspaces.term2term')
 require('workspaces.defualt')
 
---
+++ modules/home/dev/wezterm/wezterm/workspaces/term2term.lua
@@ -0,0 +1,47 @@
+local wezterm = require 'wezterm'
+
+
+local mux = wezterm.mux
+wezterm.on('gui-startup', function(cmd)
+    local args = {}
+    if cmd then
+        args = cmd.args
+    end
+
+
+    local t2t_dir = wezterm.home_dir .. '/dev/personal/term2term'
+    local rs_t, rs_p, rs_w = mux.spawn_window {
+        workspace = 'term2term',
+        cwd = t2t_dir .. '/client',
+        args = args
+    }
+
+    rs_t:send_text 'nvim .\n'
+    rs_t:set_title 'rs'
+
+    local go_t, go_p, go_w = rs_w:spawn_tab {
+        cwd = t2t_dir .. '/server',
+    }
+
+    go_t:send_text 'nvim .\n'
+    go_t:set_title 'go'
+
+    local build_t, build_p, build_w = go_w:spawn_tab {
+        cwd = t2t_dir .. '/client/'
+    }
+
+    local dev_server_p = build_p:split {
+        direction = 'Left',
+        size = 0.5,
+        cwd = t2t_dir .. '/client/backend/cmd/t2tServer'
+    }
+
+
+    build_tab:set_title 'build'
+
+    dev_server_p:send_text 'cargo run --'
+
+    build_p:send_text 'go run .'
+
+    tab:activate()
+end)
