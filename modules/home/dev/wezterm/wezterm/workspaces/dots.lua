7b9b5624dc4731c71a77eccecbc448fc7916a35f
suri312006 <suri312006@gmail.com>
2024-07-16 03:41:27 -0700

    Generation: '710'

--- modules/home/dev/wezterm/wezterm/workspaces/dots.lua
+++ modules/home/dev/wezterm/wezterm/workspaces/dots.lua
@@ -17,15 +17,13 @@ wezterm.on('gui-startup', function(cmd)
         args = args
     }
     nix_dots_t:set_title 'nixdots'
-    nix_dots_p:send_text 'nvim .'
-    -- nix_dots_p:send_text 'nvim .\n'
+    nix_dots_p:send_text 'nvim .\n'
 
     local nvim_t, nvim_p, nvim_w = nix_dots_w:spawn_tab {
         cwd = dots_dir .. '/nvim'
     }
     nvim_t:set_title 'nvim'
     -- nvim_p:send_text 'nvim .\n'
-    nvim_p:send_text 'nvim .'
 
     local build_t, build_p, build_w = nvim_w:spawn_tab {
         cwd = dots_dir .. '/nvim'
