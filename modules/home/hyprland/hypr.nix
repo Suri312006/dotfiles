5cbc3b28c765f56e6644ef7cb51b1dc08395c332
suri312006 <suri312006@gmail.com>
2024-07-19 13:06:45 -0700

    Generation: '738'

--- modules/home/hyprland/hypr.nix
+++ modules/home/hyprland/hypr.nix
@@ -39,7 +39,7 @@
 
       monitor = [
         "eDP-1, 1920x1080@165.00, 0x0, 1"
-        "DP-3, 1920x1080@74.97, 1920x0, 1"
+        # "DP-3, 1920x1080@74.97, 1920x0, 1"
         "HDMI-A-1, 1920x1080@74.97, 1920x0, 1"
         # "DP-3, preferred, auto, 1, transform, 1"
       ];
@@ -195,11 +195,11 @@
       ];
 
       workspace = [
-        "1, monitor:DP-3"
-        "2, monitor:DP-3"
-        "3, monitor:DP-3"
-        "4, monitor:DP-3"
-        "5, monitor:DP-3"
+        # "1, monitor:DP-3"
+        # "2, monitor:DP-3"
+        # "3, monitor:DP-3"
+        # "4, monitor:DP-3"
+        # "5, monitor:DP-3"
 
         "1, monitor:HDMI-A-1"
         "2, monitor:HDMI-A-1"
