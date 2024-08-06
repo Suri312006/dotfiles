10378284163cd554c9d770cd38512d543fa27384
suri312006 <suri312006@gmail.com>
2024-08-06 01:30:39 -0700

    Generation: '762'

--- modules/home/dev/wezterm/wezterm/workspaces/init.lua
+++ modules/home/dev/wezterm/wezterm/workspaces/init.lua
@@ -3,6 +3,5 @@ require('workspaces.school')
 require('workspaces.dots')
 require('workspaces.connectify')
 require('workspaces.term2term')
-require('workspaces.lightlinks')
 require('workspaces.default')
 
