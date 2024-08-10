c528ea2ba73c9b6f6ca98949266d262f1ab8c3ec
suri312006 <suri312006@gmail.com>
2024-08-10 14:34:14 -0700

    Generation: '776'

--- modules/home/hyprland/hypr.nix
+++ modules/home/hyprland/hypr.nix
@@ -113,6 +113,7 @@
         "noinitialfocus,class:^(xwaylandvideobridge)$"
         "maxsize 1 1,class:^(xwaylandvideobridge)$"
         "noblur,class:^(xwaylandvideobridge)$"
+        "noblur,class:^(coupled-cats)$"
       ];
 
 
