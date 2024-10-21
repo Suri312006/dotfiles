107b5ee68ccadf5b7a7f877ebf3758fa5820487d
suri312006 <suri312006@gmail.com>
2024-10-20 22:59:22 -0700

    Generation: '831'

--- modules/home/dev/zsh/default.nix
+++ modules/home/dev/zsh/default.nix
@@ -76,7 +76,7 @@ in
 
 
 
-      function y() {
+      function n() {
       	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
       	yazi "$@" --cwd-file="$tmp"
       	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
