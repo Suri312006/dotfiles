d65f6bf0b2c2e1d369721cfa4b701acf0be28a97
suri312006 <suri312006@gmail.com>
2024-10-22 13:12:42 -0700

    Generation: '846'

--- modules/home/dev/nvim.nix
+++ modules/home/dev/nvim.nix
@@ -1,24 +1,25 @@
-{ pkgs, fetchFromGitHub, ... }: {
-  programs = {
-    zsh.shellAliases.n = "nvim .";
-    zsh.shellAliases.nv = "nvim";
-
-    neovim = {
-      enable = true;
-      defaultEditor = true;
-      extraPackages = with pkgs; [
-        ripgrep
-        fd
-      ];
-    };
-
-  };
-
-  home.file.".config/nvim" = {
-    source = builtins.fetchGit {
-      url = "https://github.com/suri312006/nvim";
-      rev = "2f6d796bb707a8f9739a62af4656d341ccf0f1cb";
-    };
-  };
-
-}
+{ }
+# { pkgs, fetchFromGitHub, ... }: {
+#   programs = {
+#     zsh.shellAliases.n = "nvim .";
+#     zsh.shellAliases.nv = "nvim";
+#
+#     neovim = {
+#       enable = true;
+#       defaultEditor = true;
+#       extraPackages = with pkgs; [
+#         ripgrep
+#         fd
+#       ];
+#     };
+#
+#   };
+#
+#   home.file.".config/nvim" = {
+#     source = builtins.fetchGit {
+#       url = "https://github.com/suri312006/nvim";
+#       rev = "2f6d796bb707a8f9739a62af4656d341ccf0f1cb";
+#     };
+#   };
+#
+# }
