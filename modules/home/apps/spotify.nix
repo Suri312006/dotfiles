62a1b9c7028af569c74490553cda253f6a6a5bfe
suri312006 <suri312006@gmail.com>
2024-10-22 18:13:14 -0700

    Generation: '847'

--- modules/home/apps/spotify.nix
+++ modules/home/apps/spotify.nix
@@ -1,28 +1,44 @@
 { pkgs, lib, inputs, ... }:
-let
-  spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
-in
 {
+programs.spicetify =
+   let
+     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+   in
+   {
+     enable = true;
+     enabledExtensions = with spicePkgs.extensions; [
+       adblock
+       hidePodcasts
+       shuffle # shuffle+ (special characters are sanitized out of extension names)
+     ];
+     theme = spicePkgs.themes.catppuccin;
+     colorScheme = "mocha";
+   }
+}
+#let
+#  spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
+#in
+#{
   # allow spotify to be installed if you don't have unfree enabled already
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "spotify"
-  ];
+  #nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    #"spotify"
+  #];
 
   # import the flake's module for your system
-  imports = [ inputs.spicetify-nix.homeManagerModule ];
+  #imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
   # configure spicetify :)
-  programs.spicetify =
-    {
-      enable = true;
-      theme = lib.mkForce spicePkgs.themes.Onepunch;
-      colorScheme = lib.mkForce "Onepunch";
+  #programs.spicetify =
+    #{
+      #enable = true;
+      #theme = lib.mkForce spicePkgs.themes.Onepunch;
+      #colorScheme = lib.mkForce "Onepunch";
 
-      enabledExtensions = with spicePkgs.extensions; [
-        fullAppDisplay
-        shuffle # shuffle+ (special characters are sanitized out of ext names)
-        hidePodcasts
-        adblock
-      ];
-    };
-}
+      #enabledExtensions = with spicePkgs.extensions; [
+        #fullAppDisplay
+        #shuffle # shuffle+ (special characters are sanitized out of ext names)
+        #hidePodcasts
+        #adblock
+      #];
+    #};
+#}
