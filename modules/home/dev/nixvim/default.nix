a930bd4c6702ac1e410d563df75a18c52972d44b
suri312006 <suri312006@gmail.com>
2024-10-22 20:32:48 -0700

    Generation: '851'

--- modules/home/dev/nixvim/default.nix
+++ modules/home/dev/nixvim/default.nix
@@ -450,7 +450,7 @@
   {
     mode = "n";
     key = "<leader>s";
-    action = [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]];
+    action = ":%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>";
     description = "Search and replace the word under cursor";
   }
 
