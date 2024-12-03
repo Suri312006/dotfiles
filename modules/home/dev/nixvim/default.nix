dd52d92bc4684e19ce99b3c046f3e90890680e18
suri312006 <suri312006@gmail.com>
2024-12-02 22:50:39 -0800

    Generation: '922'

--- modules/home/dev/nixvim/default.nix
+++ modules/home/dev/nixvim/default.nix
@@ -495,7 +495,7 @@
         hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
       end, {remap=true})
       vim.keymap.set('n', '<C-f>', function()
-        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+        hop.hint_char1({ current_line_only = false })
       end, {remap=true})
 
 
