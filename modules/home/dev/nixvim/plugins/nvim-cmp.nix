4b3852a17df8f0d9a492a0a2d394fc384a59ef00
suri312006 <suri312006@gmail.com>
2024-12-02 21:43:17 -0800

    Generation: '916'

--- modules/home/dev/nixvim/plugins/nvim-cmp.nix
+++ modules/home/dev/nixvim/plugins/nvim-cmp.nix
@@ -34,8 +34,8 @@
         mapping = {
           # Select the [n]ext item
           "<C-n>" = "cmp.mapping.select_next_item()";
-          "<down>" = "cmp.mapping.select_prev_item()";
-          "<up>" = "cmp.mapping.select_next_item()";
+          "<up>" = "cmp.mapping.select_prev_item()";
+          "<dowm>" = "cmp.mapping.select_next_item()";
           # Select the [p]revious item
           "<C-p>" = "cmp.mapping.select_prev_item()";
           # Scroll the documentation window [b]ack / [f]orward
