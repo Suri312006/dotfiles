ce7b7b8415de120217592b67c4af64653a93f5d8
suri312006 <suri312006@gmail.com>
2024-12-02 20:36:17 -0800

    Generation: '907'

--- modules/home/dev/zsh/default.nix
+++ modules/home/dev/zsh/default.nix
@@ -49,9 +49,9 @@ in {
     shellAliases = {
       re = ''
         VERSION=$(($(readlink /nix/var/nix/profiles/system | grep -o "[0-9]*") + 1)) 
-        z ~/dots/nixdots && git add -A && git commit -m "Generation: '$VERSION'" && sudo nixos-rebuild switch --flake /home/suri/dots/nixdots#zephryus'';
+        z ~/dots/nixdots && git add -A && git commit -m "Generation: '$VERSION'" && sudo nixos-rebuild switch --flake /home/suri/dots/nixdots#zephryus && git push'';
 
-      ree = ''sudo nixos-rebuild switch --flake ~/dots/nixdots#zephryus'';
+      ree = ''sudo nixos-rebuild switch --flake ~/dots/nixdots#zephryus && git push'';
 
       dots = "z ~/dots";
       fcd = ''cd "$(find ~/coding/ ~/storage/ -type d -not \( -path "*/.git/*" -o -path "*/target/*" -o -path "*/.venv/*" -o -path "*/node_modules/*" -o -path "*/venv/*" -o -path "*/build/*" -o -path "*/.*/*" \) -print 2>/dev/null | fzf)" '';
