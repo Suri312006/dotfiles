6cdef6857febc8c3cee3319bdfb52e5bdc1558fe
suri312006 <suri312006@gmail.com>
2024-12-02 19:33:18 -0800

    Generation: '899'

--- modules/system/extra/fonts.nix
+++ modules/system/extra/fonts.nix
@@ -1,11 +1,9 @@
-{ pkgs, ... }:
-{
-
+{pkgs, ...}: {
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-    rPackages.phosphoricons
+    # (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+    # nerd
+    # rPackages.phosphoricons
+    nerd-fonts.fira-code
     monaspace
   ];
-
 }
-
