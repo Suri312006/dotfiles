c900c69745b41b027b8a30518c223edf78da63e3
suri312006 <suri312006@gmail.com>
2024-12-26 12:13:59 -0800

    Generation: '960'

--- modules/system/core/sound.nix
+++ modules/system/core/sound.nix
@@ -1,13 +1,40 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
+  # hardware.pulseaudio.package = pkgs.pulseaudioFull;
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = false;
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  # };
 
-  hardware.pulseaudio.package = pkgs.pulseaudioFull;
-#  sound.enable = true;
-  hardware.pulseaudio.enable = false;
+  # Remove sound.enable or set it to false if you had it set previously, as sound.enable is only meant for ALSA-based configurations
+
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    jack.enable = true;
   };
 
+  # bluetooth stuff
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.blueman.enable = true;
+  services.pipewire.wireplumber.extraConfig = {
+    "monitor.bluez.properties" = {
+      "bluez5.enable-sbc-xq" = true;
+      "bluez5.enable-msbc" = true;
+      "bluez5.enable-hw-volume" = true;
+      "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+    };
+  };
 }
