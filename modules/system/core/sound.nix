d9997848b5cac8297e0872017fbf0f38fe043016
suri312006 <suri312006@gmail.com>
2025-01-02 08:48:08 -0800

    Generation: '970'

--- modules/system/core/sound.nix
+++ modules/system/core/sound.nix
@@ -12,29 +12,31 @@
   # Remove sound.enable or set it to false if you had it set previously, as sound.enable is only meant for ALSA-based configurations
 
   # rtkit is optional but recommended
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-  };
-
-  # bluetooth stuff
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
-
-  services.blueman.enable = true;
-  services.pipewire.wireplumber.extraConfig = {
-    "monitor.bluez.properties" = {
-      "bluez5.enable-sbc-xq" = true;
-      "bluez5.enable-msbc" = true;
-      "bluez5.enable-hw-volume" = true;
-      "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
-    };
-  };
+  #   security.rtkit.enable = true;
+  #   services.pipewire = {
+  #     enable = true;
+  #     alsa.enable = true;
+  #     alsa.support32Bit = true;
+  #     pulse.enable = true;
+  #     # If you want to use JACK applications, uncomment this
+  #     jack.enable = true;
+  #   };
+  #
+  #   # bluetooth stuff
+  #   hardware.bluetooth = {
+  #     enable = true;
+  #     powerOnBoot = true;
+  #   };
+  #
+  #   services.blueman.enable = true;
+  #   services.pipewire.wireplumber.extraConfig = {
+  #     "monitor.bluez.properties" = {
+  #       "bluez5.enable-sbc-xq" = true;
+  #       "bluez5.enable-msbc" = true;
+  #       "bluez5.enable-hw-volume" = true;
+  #       "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+  #     };
+  #   };
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
 }
