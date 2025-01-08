35580d39866be6ed5c7266e68e7af50a33cb3b81
suri312006 <suri312006@gmail.com>
2025-01-08 03:54:09 -0800

    Generation: '990'

--- modules/home/hyprland/hypr.nix
+++ modules/home/hyprland/hypr.nix
@@ -39,12 +39,12 @@
         "spotify &"
         "zeditor &"
 
-        "hyprctl keyword dwindle:permanent_direction_override true"
-        "hyprctl dispatch layoutmsg preselect t"
+        "hyprctl keyword dwindle:permanent_direction_override true&"
+        "hyprctl dispatch layoutmsg preselect t&"
         "discord &"
         "zulip &"
         "slack && sleep 1 &"
-        "hyprctl keyword dwindle:permanent_direction_override false"
+        "hyprctl keyword dwindle:permanent_direction_override false&"
 
         # "sleep 1 && slack &" # time delay for slack
         "wl-paste --type text --watch cliphist store"
