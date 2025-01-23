594bbe2cf1d011476c41288cc561f58ced739b85
suri312006 <suri312006@gmail.com>
2025-01-23 12:08:34 -0800

    Generation: '1007'

--- modules/home/dev/ghostty/default.nix
+++ modules/home/dev/ghostty/default.nix
@@ -2,7 +2,7 @@
   home.packages = [ inputs.ghostty.packages.x86_64-linux.default ];
 
   home.file.".config/ghostty/config".text = ''
-    font-family=""
-    font-family= "Monaspace Krypton Var SemiBold"
+    font-family = ""
+    font-family = "Monaspace Krypton Var SemiBold"
   '';
 }
