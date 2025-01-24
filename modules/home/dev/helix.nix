fedf8c1ff2b97d25d6aa088c9c860daa591d4930
suri312006 <suri312006@gmail.com>
2025-01-23 22:29:14 -0800

    Generation: '1024'

--- modules/home/dev/helix.nix
+++ modules/home/dev/helix.nix
@@ -3,7 +3,6 @@
     enable = true;
 
     settings = {
-      # theme = "autumn_night_transparent";
       editor.cursor-shape = {
         normal = "block";
         insert = "bar";
@@ -11,17 +10,14 @@
       };
 
       editor.line-number = "relative";
+      editor.inline-diagnostics.cursor-line = "warning";
+      editor.end-of-line-diagnostics = "hint";
     };
+
     languages.language = [{
       name = "nix";
       auto-format = true;
       formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
     }];
-    # themes = {
-    #   autumn_night_transparent = {
-    #     # "inherits" = "autumn_night";
-    #     "ui.background" = { };
-    #   };
-    # };
   };
 }
