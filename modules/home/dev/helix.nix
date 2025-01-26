d0a66d93d1f6d874365a972fade4b8145878f385
suri312006 <suri312006@gmail.com>
2025-01-25 16:41:00 -0800

    Generation: '1027'

--- modules/home/dev/helix.nix
+++ modules/home/dev/helix.nix
@@ -1,6 +1,21 @@
 { pkgs, ... }: {
   programs.helix = {
     enable = true;
+    defaultEditor = true;
+    extraPackages = with pkgs; [
+      markdown-oxide
+      gopls
+      nil
+      texlab
+      taplo
+      lua-language-server
+      haskell-language-server
+      python312Packages.python-python-lsp-server
+      typescript-language-server
+      svelte-language-server
+      tailwindcss-language-server
+      emmet-language-server
+    ];
 
     settings = {
       editor.cursor-shape = {
@@ -9,15 +24,262 @@
         select = "underline";
       };
 
-      editor.line-number = "relative";
-      editor.inline-diagnostics.cursor-line = "warning";
-      editor.end-of-line-diagnostics = "hint";
+      editor = {
+        line-number = "relative";
+        inline-diagnostics.cursor-line = "warning";
+        end-of-line-diagnostics = "hint";
+
+        auto-save = { focus-lost = true; };
+
+        statusline = {
+          left = [
+            "mode"
+            "spinner"
+            "file-name"
+            "file-type"
+            "total-line-numbers"
+            "file-encoding"
+          ];
+          right = [
+            "selections"
+            "primary-selection-length"
+            "position"
+            "position-percentage"
+            "spacer"
+            "diagnostics"
+            "workspace-diagnostics"
+            "version-control"
+          ];
+          separator = "│";
+          mode.normal = "NORMAL";
+          mode.insert = "INSERT";
+          mode.select = "SELECT";
+        };
+      };
+
     };
 
-    languages.language = [{
-      name = "nix";
-      auto-format = true;
-      formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-    }];
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }
+
+      {
+        name = "svelte";
+        auto-format = true;
+        formatter = {
+          command = "dprint";
+          args = [ "fmt" "--stdin" "svelte" ];
+        };
+
+        language-servers = [
+          # "emmet-lsp"
+          "svelteserver"
+          # "tailwindcss-ls"
+          "tailwindcss-language-server"
+
+          # "typescript-language-server"
+        ];
+      }
+      {
+        name = "html";
+        language-servers = [ "vscode-html-language-server" "tailwindcss-ls" ];
+      }
+      {
+        name = "css";
+        language-servers = [ "vscode-css-language-server" "tailwindcss-ls" ];
+      }
+    ];
   };
 }
+
+# { pkgs, ... }:
+
+# {
+#   programs.helix = {
+#     enable = true;
+#     defaultEditor = true;
+#     extraPackages = with pkgs; [
+#       markdown-oxide
+#       # marksman
+#       gopls
+#       nil
+#       texlab
+#       taplo
+#       lua-language-server
+#       haskell-language-server
+
+#       python312Packages.python-lsp-server
+#       vscode-langservers-extracted # html/css/json/eslint
+#       typescript-language-server
+#       svelte-language-server
+#       tailwindcss-language-server
+#       emmet-language-server
+#       dprint # formatter
+#     ];
+
+#     # [[language]]
+#     # name = "mylang"
+#     # scope = "source.mylang"
+#     # injection-regex = "mylang"
+#     # file-types = ["mylang", "myl"]
+#     # comment-tokens = "#"
+#     # indent = { tab-width = 2, unit = "  " }
+#     # formatter = { command = "mylang-formatter" , args = ["--stdin"] }
+#     # language-servers = [ "mylang-lsp" ]
+#     languages = {
+#       language = [
+#         {
+#           name = "haskell";
+#           file-types = ["lc" "hs" "lhs"];
+#           language-servers = [
+#             "haskell-language-server"            
+#           ];
+#         }
+#         {
+#           name = "c";
+#           auto-format = true;
+#           formatter = {
+#             command = "clang-format";
+#           };
+#           indent = { tab-width = 4; unit = "    "; };
+#         }
+#         {
+#           name = "rust";
+#           auto-format = true;
+#           formatter = {
+#             command = "cargo fmt";
+#           };
+#           indent = { tab-width = 4; unit = "    "; };
+#         }
+#         {
+#           name = "svelte";
+#           auto-format = true;
+#           formatter = { 
+#             command = "dprint"; 
+#             args = ["fmt" "--stdin" "svelte"];
+#           };
+
+#           language-servers = [
+#             # "emmet-lsp"
+#             "svelteserver"
+#             # "tailwindcss-ls"
+#             "tailwindcss-language-server"
+
+#             # "typescript-language-server"
+#           ];
+#         }
+#         {
+#           name = "html";
+#           language-servers = [ "vscode-html-language-server" "tailwindcss-ls" ];
+#         }
+#         {
+#           name = "css";
+#           language-servers = [ "vscode-css-language-server" "tailwindcss-ls" ];
+#         }
+#       ];
+
+#       language-server = {       
+#         emmet-lsp = {
+#           command = "emmet-language-server";
+#           args = ["--stdio"];
+#         };
+#         tailwind-ls = {
+#           command = "tailwindcss-language-server";
+#           args = ["--stdio"];
+#         };
+#       #   golps = {
+#       #     command = "gopls;
+#       #   };
+#       };
+
+#     };
+#     settings = {
+#       # theme = "kanagawa";
+#       theme = "o2";
+
+#       # theme = "ayu_evolve";
+#       # voxed, gruvbox_dark_hard, tokyonight, doom_acario_dark, kaolin_valley_dark, ayu_dark, material_deep_ocean, monokai_pro_octagon, poimandres, starlight, term16_dark, varua 
+#       editor = {
+#         auto-save = {
+#           focus-lost = true;
+#           # after-delay.enable = true;
+#           # after-delay.timeout = 300;
+#         };
+#         auto-format = true;
+#         bufferline = "multiple";
+#         color-modes = true;
+#         cursorline = true;
+#         cursor-shape = {
+#           insert = "bar";
+#           normal = "block";
+#           select = "underline";
+#         };
+#         file-picker.hidden = false;
+#         idle-timeout = 50;
+#         indent-guides = {
+#           render = true;
+#           character = " ";
+#         };
+#         line-number = "relative";
+#         lsp.display-messages = true;
+#         scrolloff = 10;
+#         shell = ["zsh" "-ic"];
+#         soft-wrap = {
+#           enable = true;
+#           # max-wrap = 25; # increase value to reduce forced mid-word wrapping
+#           # max-indent-retain = 0; max indent to carry over when soft wrapping a line.
+#           # wrap-indicator = "";  # set wrap-indicator to "" to hide it
+#         };
+#         statusline = {
+#           # left = ["mode" "spacer"  "spinner"  "spacer"  "version-control"  "file-name"  "file-modification-indicator"];
+#           # right = ["diagnostics"  "version-control"  "selections"  "position"  "total-line-numbers"  "file-encoding"  "file-type"];
+#           left = ["mode" "spinner" "file-name" "file-type" "total-line-numbers" "file-encoding"];
+#           right = ["selections" "primary-selection-length" "position" "position-percentage" "spacer" "diagnostics" "workspace-diagnostics" "version-control"];
+#           separator = "│";
+#           mode.normal = "NORMAL";
+#           mode.insert = "INSERT";
+#           mode.select = "SELECT";
+#         };
+#       };
+#       # keys.picker = { # doesn't exist yet 😭
+#       #   "C-k" = "previous_entry";
+#       #   "C-j" = "next_entry";
+#       # };
+#       keys.normal.C-y = {
+#         # Open the file(s) in the current window
+#         y = ":sh zellij run -c -f -x 10% -y 10% --width 80% --height 80% -- bash $DOT/helix/yazi-picker.sh open";
+#         # Open the file(s) in a vertical pane
+#         v = ":sh zellij run -c -f -x 10% -y 10% --width 80% --height 80% -- bash $DOT/helix/yazi-picker.sh vsplit";
+#         # Open the file(s) in a horizontal pane
+#         h = ":sh zellij run -c -f -x 10% -y 10% --width 80% --height 80% -- bash $DOT/helix/yazi-picker.sh hsplit";
+#       };
+#       keys.normal = {
+#         esc = [ "collapse_selection" "keep_primary_selection" ];
+#         y = ["yank" "yank_to_clipboard"];
+#         d = ["delete_selection" "yank_to_clipboard"];
+
+#         # C-y = ":sh zellij run -c -f -x 10% -y 10% --width 80% --height 80% -- bash ~/.config/helix/yazi-picker.sh open";
+
+#         j = [ "move_line_down" ];
+#         k = [ "move_line_up" ];
+#         # J = [ "move_visual_line_down" "align_view_center" ];
+#         # K = [ "move_visual_line_up" "align_view_center" ];
+#         X = "extend_line_above";
+#         "{" = [ "goto_prev_paragraph" "collapse_selection"];
+#         "}" = [ "goto_next_paragraph" "collapse_selection"];
+
+#         C-j = ["extend_to_line_bounds"  "delete_selection"  "paste_after"];
+#         C-k = ["extend_to_line_bounds"  "delete_selection"  "move_line_up"  "paste_before"];
+#         C-n = ["scroll_down"];
+#         C-p = ["scroll_up"];
+
+#         space = {
+#           W = ":write";
+#         };
+#       };
+#     };
+#   };
+# }
