4e30b09db2d84a24b95b0d2449c2e84b641a949e
suri312006 <suri312006@gmail.com>
2025-02-04 22:52:22 -0800

    Generation: '1061'

--- modules/home/dev/wezterm/wezterm/workspaces/cse108.lua
+++ modules/home/dev/wezterm/wezterm/workspaces/cse108.lua
@@ -11,7 +11,10 @@ wezterm.on('gui-startup', function(cmd)
     if cmd then
         args = cmd.args
     end
-    local dev_dir = wezterm.home_dir .. '/dev/school/cse108'
+
+    local class_dir = wezterm.home_dir .. 'dev/school/cse108'
+    
+    local dev_dir = class_dir .. '/hermes'
 
     local tab, pane, window = mux.spawn_window {
         workspace = 'cse108',
@@ -22,11 +25,15 @@ wezterm.on('gui-startup', function(cmd)
     tab:set_title "Helix"
     pane:send_text "hx .\n"
 
+    local b_t, b_p, b_w = window:spawn_tab({
+        cwd = dev_dir
+    })
+
+    b_p:send_text "just "
+
+    b_t:set_title "Build"
+
     tab:activate()
-    
-
-
-
 end)
 
 return {}
