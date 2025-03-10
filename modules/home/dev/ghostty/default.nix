93ae60f6251993930f71e1d71ed436d03c55ab2a
suri312006 <suri312006@gmail.com>
2025-03-09 18:56:07 -0700

    Generation: '1103'

--- modules/home/dev/ghostty/default.nix
+++ modules/home/dev/ghostty/default.nix
@@ -1,13 +1,41 @@
 { inputs, ... }: {
   home.packages = [ inputs.ghostty.packages.x86_64-linux.default ];
 
+  # what
+  # === != <= ->
+
   home.file.".config/ghostty/config".text = ''
-     font-family = ""
-     font-family = "Monaspace Krypton SemiBold"
-     font-size = "11"
-     window-padding-x = 4
-     window-padding-y = 3,1
-     theme = "gruvbox-material"
-    # initial-command = "zellij"
+
+    font-family = ""
+
+    font-family = "Monaspace Krypton SemiBold"
+
+    font-family-bold = "Monaspace Neon SemiBold"
+
+    font-family-italic = "Monaspace Xenon SemiBold"
+
+    font-family-bold-italic = "Monaspace Radon SemiBold"
+
+
+    font-feature = "calt"
+    font-feature = "clig"
+    font-feature = "liga"
+    font-feature = "dlig"
+    font-feature = "ss01"
+    font-feature = "ss02"
+    font-feature = "ss03"
+    font-feature = "ss04"
+    font-feature = "ss05"
+    font-feature = "ss06"
+    font-feature = "ss07"
+    font-feature = "ss08"
+
+    font-size = "10"
+
+    window-padding-x = 4
+
+    window-padding-y = 3,1
+
+    theme = "gruvbox-material"
   '';
 }
