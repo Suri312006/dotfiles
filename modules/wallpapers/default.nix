b5dac7a1a5a72465ce07c99433d08575594a43d7
suri312006 <suri312006@gmail.com>
2025-03-11 00:04:56 -0700

    Generation: '1111'

--- modules/wallpapers/default.nix
+++ modules/wallpapers/default.nix
@@ -1,4 +1,5 @@
 { ... }: {
 
-  home.file."Pictures/curr_wall.png".source = ./a_video_game_screen_capture.jpg;
+  home.file."Pictures/curr_wall.png".source =
+    ./a_drawing_of_an_astronaut_in_space.png;
 }
