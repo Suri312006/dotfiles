e51205d71e3942c8872ca965e931f348d5aa286c
suri312006 <suri312006@gmail.com>
2025-03-20 00:55:28 -0700

    Generation: '1125'

--- modules/home/dev/starship/default.nix
+++ modules/home/dev/starship/default.nix
@@ -2,25 +2,29 @@
 
   programs.starship = {
     enable = true;
-    settings = lib.mkForce {
+    settings = {
       "$schema" = "https://starship.rs/config-schema.json";
 
       format =
         "[](color_orange)$os$username[](bg:color_yellow fg:color_orange)$directory[](fg:color_yellow bg:color_aqua)$git_branch$git_status[](fg:color_aqua bg:color_blue)$c$rust$golang$nodejs$php$java$kotlin$haskell$python[](fg:color_blue bg:color_bg3)$docker_context$conda[](fg:color_bg3 bg:color_bg1)$time[ ](fg:color_bg1)$line_break$character";
 
+      # Palette definition needs to be structured correctly
       palette = "gruvbox_dark";
 
-      "palettes.gruvbox_dark" = {
-        color_fg0 = "#fbf1c7";
-        color_bg1 = "#3c3836";
-        color_bg3 = "#665c54";
-        color_blue = "#458588";
-        color_aqua = "#689d6a";
-        color_green = "#98971a";
-        color_orange = "#d65d0e";
-        color_purple = "#b16286";
-        color_red = "#cc241d";
-        color_yellow = "#d79921";
+      # Palettes need to be defined as a top-level key
+      palettes = {
+        gruvbox_dark = {
+          color_fg0 = "#fbf1c7";
+          color_bg1 = "#3c3836";
+          color_bg3 = "#665c54";
+          color_blue = "#458588";
+          color_aqua = "#689d6a";
+          color_green = "#98971a";
+          color_orange = "#d65d0e";
+          color_purple = "#b16286";
+          color_red = "#cc241d";
+          color_yellow = "#d79921";
+        };
       };
 
       os = {
@@ -160,16 +164,7 @@
 
       line_break = { disabled = false; };
 
-      character = {
-        disabled = false;
-        # Commented out the following lines, as they might cause issues
-        # success_symbol = "[](bold fg:color_green)";
-        # error_symbol = "[](bold fg:color_red)";
-        # vimcmd_symbol = "[](bold fg:color_green)";
-        # vimcmd_replace_one_symbol = "[](bold fg:color_purple)";
-        # vimcmd_replace_symbol = "[](bold fg:color_purple)";
-        # vimcmd_visual_symbol = "[](bold fg:color_yellow)";
-      };
+      character = { disabled = false; };
     };
   };
 }
