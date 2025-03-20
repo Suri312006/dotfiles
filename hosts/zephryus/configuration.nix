6e998e234d0d3c9035af86a1ccfe7b0287e9b1a5
suri312006 <suri312006@gmail.com>
2025-03-20 00:21:02 -0700

    Generation: '1123'

--- hosts/zephryus/configuration.nix
+++ hosts/zephryus/configuration.nix
@@ -22,6 +22,12 @@
 
   programs.rog-control-center.enable = true;
 
+  # nix.settings = {
+  #   substituters = [ "https://hyprland.cachix.org" ];
+  #   trusted-public-keys =
+  #     [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+  # };
+
   nix.settings = {
     substituters = [ "https://hyprland.cachix.org" ];
     trusted-public-keys =
