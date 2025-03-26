35f0e30e4cd7bba5c3a51a4ba93acf14d413b957
suri312006 <suri312006@gmail.com>
2025-03-26 15:41:20 -0700

    Generation: 1139

--- modules/home/dev/fish.nix
+++ modules/home/dev/fish.nix
@@ -8,6 +8,7 @@
     enable = true;
 
     interactiveShellInit = ''
+      eval `(ssh-agent)`
       ssh-add ~/.ssh/github_private &> /dev/null
       ssh-add ~/.ssh/ucsc_gitlab &> /dev/null
       ssh-add ~/.ssh/connectify &> /dev/null
