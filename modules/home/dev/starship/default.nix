e9f5f9ebafb8c0488b927e4beab6a28d3be18609
suri312006 <suri312006@gmail.com>
2025-03-26 19:23:26 -0700

    Generation: 1147

--- modules/home/dev/starship/default.nix
+++ modules/home/dev/starship/default.nix
@@ -3,8 +3,8 @@
   programs.starship = {
     enable = true;
 
-    # settings =
-    #   lib.mkForce (builtins.fromTOML (builtins.readFile ./starship.toml));
+    settings =
+      lib.mkForce (builtins.fromTOML (builtins.readFile ./starship.toml));
   };
   # home.file.".config/starship.toml".source = lib.mkForce ./starship.toml;
 
