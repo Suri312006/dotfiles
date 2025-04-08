05742f03bb3b955418395dc306395528486bb8f8
suri312006 <suri312006@gmail.com>
2025-04-08 12:17:52 -0700

    Generation: 1170

--- hosts/zephryus/configuration.nix
+++ hosts/zephryus/configuration.nix
@@ -46,23 +46,26 @@
     enable = true;
     setSocketVariable = true;
   };
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-      ovmf = {
-        enable = true;
-        packages = [
-          (pkgs.OVMF.override {
-            secureBoot = true;
-            tpmSupport = true;
-          }).fd
-        ];
-      };
-    };
-  };
+
+  # just for cse150
+  boot.blacklistedKernelModules = [ "kvm" "kvm_amd" ];
+  # virtualisation.libvirtd = {
+  #   enable = true;
+  #   qemu = {
+  #     package = pkgs.qemu_kvm;
+  #     runAsRoot = true;
+  #     swtpm.enable = true;
+  #     ovmf = {
+  #       enable = true;
+  #       packages = [
+  #         (pkgs.OVMF.override {
+  #           secureBoot = true;
+  #           tpmSupport = true;
+  #         }).fd
+  #       ];
+  #     };
+  #   };
+  # };
 
   home-manager = {
     extraSpecialArgs = { inherit inputs outputs; };
