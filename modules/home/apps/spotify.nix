6b4d8c4b7e1cda29cfd79711d7dc3b9146076b1b
suri312006 <suri312006@gmail.com>
2025-05-14 16:47:35 -0700

    Generation: 1193

--- modules/home/apps/spotify.nix
+++ modules/home/apps/spotify.nix
@@ -9,7 +9,7 @@
         hidePodcasts
         shuffle # shuffle+ (special characters are sanitized out of extension names)
       ];
-      theme = spicePkgs.themes.catppuccin;
+      theme = lib.mkForce spicePkgs.themes.catppuccin;
       #colorScheme = "mocha";
     };
 }
