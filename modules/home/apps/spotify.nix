49ab0cb30e5bcb9f051542881bd1c260114e5025
suri312006 <suri312006@gmail.com>
2025-05-14 16:46:55 -0700

    Generation: 1193

--- modules/home/apps/spotify.nix
+++ modules/home/apps/spotify.nix
@@ -1,22 +1,17 @@
-{
-  pkgs,
-  lib,
-  inputs,
-  ...
-}: {
-  imports = [inputs.spicetify-nix.homeManagerModules.default];
-  programs.spicetify = let
-    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-  in {
-    enable = true;
-    enabledExtensions = with spicePkgs.extensions; [
-      adblock
-      hidePodcasts
-      shuffle # shuffle+ (special characters are sanitized out of extension names)
-    ];
-    #theme = spicePkgs.themes.catppuccin;
-    #colorScheme = "mocha";
-  };
+{ pkgs, lib, inputs, ... }: {
+  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
+  programs.spicetify =
+    let spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in {
+      enable = true;
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+      ];
+      theme = spicePkgs.themes.catppuccin;
+      #colorScheme = "mocha";
+    };
 }
 #let
 #  spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
